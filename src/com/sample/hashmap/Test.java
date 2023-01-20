4f41d5db8a3f2671f224a444c30e2ca107d894b3
Dinesh Kumar <dinesh.prasad@collinsongroup.com>
2023-01-21 04:30:28 +0800

    First Commit.

--- src/com/sample/hashmap/Test.java
+++ src/com/sample/hashmap/Test.java
@@ -9,12 +9,8 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class Test {
-
 	public static void main(String[] args) {
-
 		HashMap<String, Integer> hm = new HashMap<String, Integer>();
-
-		// enter data into hashmap
 		hm.put("Math", 98);
 		hm.put("Data Structure", 85);
 		hm.put("Database", 91);
@@ -22,28 +18,24 @@ public class Test {
 		hm.put("Operating System", 79);
 		hm.put("Networking", 80);
 		Map<String, Integer> hm1 = sortByValue(hm);
+		System.out.println(hm1);
 	}
 
 	private static Map<String, Integer> sortByValue(HashMap<String, Integer> hm) {
-		// TODO Auto-generated method stub
-
 		List<Map.Entry<String, Integer>> list = new LinkedList<>(hm.entrySet());
-		
 		System.out.println(list);
 		Collections.sort(list, new Comparator<Entry<String, Integer>>() {
 
 			@Override
 			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
-				// TODO Auto-generated method stub
 				return o1.getValue().compareTo(o2.getValue());
 			}
-
-			
 		});
-		
-		
 		System.out.println(list);
-		System.out.println(hm);
-		return null;
+		Map<String, Integer> tempMain = new LinkedHashMap<>();
+		for (Entry<String, Integer> entry : list) {
+			tempMain.put(entry.getKey(), entry.getValue());
+		}
+		return tempMain;
 	}
 }
