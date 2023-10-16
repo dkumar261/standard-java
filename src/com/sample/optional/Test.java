87ae8c3f317f15794fecff07b6151009fe1d9b73
Kumar <Dinesh.Kumar28@dellteam.com>
2023-10-16 17:02:17 +0530

    Adding Test class for Optinal-class

--- src/com/sample/optional/Test.java
+++ src/com/sample/optional/Test.java
@@ -1,5 +1,27 @@
 package com.sample.optional;
 
+import java.util.Optional;
+
+class OptionalDemo {
+
+	static public Optional<String> getValue() {
+
+		String value = "This example of optional";
+		Optional<String> optional = Optional.ofNullable(value);
+		return optional;
+	}
+}
+
 public class Test {
 
+	public static void main(String[] args) {
+
+		Optional<String> value = OptionalDemo.getValue();
+		String stringValue = value.get();
+		System.out.println(stringValue);
+		
+		String getValue = value.orElse("Default value");
+		System.out.println(getValue);
+
+	}
 }
